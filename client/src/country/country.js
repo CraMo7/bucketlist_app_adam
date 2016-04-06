e09f5a4dd3d6581921917460bf222053dea1555d
Craig Morton <CraigMorton92@gmail.com>
2016-04-06 14:38:27 +0100

    adds basic index.html - adds INDEX action route @ /countries

---
+++ client/src/country/country.js
@@ -0,0 +1,6 @@
+var Country = function(){
+
+};
+
+
+module.exports = Country;
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var app = express();
 var path = require('path'); // node naitive module
 var MongoClient = require("mongodb").MongoClient;
 var bodyParser = require("body-parser");
+var url = "mongodb://localhost:27017/bucket";
 
 app.use(bodyParser.json());
 
@@ -10,6 +11,20 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname + '/client/build/index.html'));
 });
   
+app.get("/countries", function(req, res){
+  MongoClient.connect(url, function(err, db){
+    if (err){
+      console.log(err);
+      return;
+    } // [end] if error check
+    var collection = db.collection("countries");
+    collection.find({}).toArray(function(err, docs){
+      res.json(docs);
+      db.close();
+    });// [end] collection find()
+  });// [end] MongoClient connect
+});// [end] route
+
 app.use(express.static('client/build'));
 // ^ lets express know where resources (bundle.js, styles.css, images etc.) are located on your server.
 
